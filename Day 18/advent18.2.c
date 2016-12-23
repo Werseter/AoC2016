03d70581064229bcd38ebf552a9cac14c2339b10
Werseter <werseter@interia.eu>
2016-12-23 20:38:10 +0100

    Added Day 18 files

---
+++ Day 18/advent18.2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define INPUT ".^.^..^......^^^^^...^^^...^...^....^^.^...^.^^^^....^...^^.^^^...^^^^.^^.^.^^..^.^^^..^^^^^^.^^^..^"
+
+char issafe(char a, char b, char c)
+{
+    char rule[4][3]={{'^', '^', '.'},
+                     {'.', '^', '^'},
+                     {'^', '.', '.'},
+                     {'.', '.', '^'}};
+    int i;
+    for(i=0;i<4;i++)
+        if(a==rule[i][0] && b==rule[i][1] && c==rule[i][2])
+            return '^';
+    return '.';
+}
+
+int main()
+{
+    char row[120]={0};
+    char row2[120]={0};
+    strcpy(row, INPUT);
+    int count=0,i,j,l=strlen(row);
+    for(i=0;i<l;i++)
+        if(row[i]=='.')
+            count++;
+    for(j=1;j<400000;j++)
+    {
+        for(i=0;i<l;i++)
+        {
+            if(i==0)
+                row2[i]=issafe('.', row[i], row[i+1]);
+            else if(i==l-1)
+                row2[i]=issafe(row[i-1], row[i], '.');
+            else
+                row2[i]=issafe(row[i-1], row[i], row[i+1]);
+            if(row2[i]=='.')
+                count++;
+        }
+        strcpy(row, row2);
+        memset(row2, 0, l);
+    }
+    printf("There are %d safe tiles in 400000 rows\n", count);
+    return 0;
+}
--- README.md
+++ README.md
@@ -20,7 +20,7 @@
 | 15  | Timing is Everything                           | [Link](/Day%2015)   | [Link](http://adventofcode.com/2016/day/15) |
 | 16  | Dragon Checksum                                | [Link](/Day%2016)   | [Link](http://adventofcode.com/2016/day/16) |
 | 17  | Two Steps Forward                              | [Link](/Day%2017)   | [Link](http://adventofcode.com/2016/day/17) |
-| 18  | Like a Rogue                                   | Not solved yet      | [Link](http://adventofcode.com/2016/day/18) |
+| 18  | Like a Rogue                                   | [Link](/Day%2018)   | [Link](http://adventofcode.com/2016/day/18) |
 | 19  | An Elephant Named Joseph                       | Not solved yet      | [Link](http://adventofcode.com/2016/day/19) |
 | 20  | Firewall Rules                                 | Not solved yet      | [Link](http://adventofcode.com/2016/day/20) |
 | 21  | Scrambled Letters and Hash                     | Not solved yet      | [Link](http://adventofcode.com/2016/day/21) |
